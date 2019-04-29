d84639f16c49d0136098fc7011739e40e532bc80
Allan Kamau <akamau41@gmail.com>
2019-04-29 09:55:09 +0300

    add indentation to projects

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-function answer() {
+var answer = function (L1, L2, L3) {
   //Make variables for the three values.
   var L1 = parseInt(document.getElementById("L1").value);
   var L2 = parseInt(document.getElementById("L2").value);
   var L3 = parseInt(document.getElementById("L3").value);
 
   var array = [L1, L2, L3];
-  //Make an empty container to add text later.
   var text;
-  //vales for an Equilateral traingle
+
+
   if (
+    L1 + L2 <= L3 ||
+    L1 + L3 <= L2 ||
+    L2 + L3 <= L1) {
+    text = " Not a Triangle";
+  }
+
+
+  else if (
+    // var text = " an Equilateral Traingle.";
     L1 === L2 &&
     L2 === L3 &&
     L3 === L1
@@ -16,41 +25,27 @@ function answer() {
     text = " an Equilateral Traingle.";
   }
 
-  //vales for an Isosceles traingle
+
+
+  //values for an Isosceles traingle
   else if (
     L1 === L3 ||
     L3 === L2 ||
     L1 !== L2
   ) {
-    alert = "an isosceles Traingle.";
+    text = "an isosceles Traingle.";
   }
-  //Second condition for an isosceles traingle
-  // else if (
-  //   L1 === L2 ||
-  //   L2 === L3 ||
-  //   L3 !== L1
-  // ) {
-  //     alert = "an isosceles Traingle.";
-  // }
-  //vales for an scalene traingle
+
+
   else if (
     L1 !== L2 &&
     L1 !== L3 &&
     L2 !== L3
   ) {
-    alert = "a scalene Traingle.";
-
-    else if (
-      (L1 + L2 <= L3) ||
-      (L1 + L3 <= L2) ||
-      (L2 + L3 <= L1)
-    ) {
-      text = " Not a Triangle";
-    }
-    else {
-      text = "Error";
-    }
-
-    alert("This are the values you entered:" + " [" + array.join() + "] " + "\nThis is " + text);
-    ")
+    text = "a scalene Traingle.";
   }
+
+
+  alert("These are the values you entered:" + " [" + array.join() + "] " + "\nThis is " + text);
+
+}
